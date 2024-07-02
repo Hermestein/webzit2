dc7f72e1cddd1d54f4dcdb7daf5a6728a84ebd2a
Hermestein <arthestd@gmail.com>
2024-07-02 10:28:22 +0700

    webzit2

---
+++ data.js
@@ -0,0 +1,29 @@
+const nama = "hermestein"
+let usia = 10;
+
+let biodata = document.getElementById('biodata');
+console.log(biodata);
+
+function generateBiodata() {
+    let generasi;
+
+    if (usia > 10 && usia < 18) {
+        generasi = "generasi remaja";
+    } 
+    else if (usia > 18 && usia <= 30) {
+        generasi = "generasi dewasa"
+    } 
+    else if(usia <10) {
+        generasi = "generasi anak anak"
+    }
+    else {
+        generasi = "generasi bayi"
+    }
+
+    return biodata.innerHTML = generasi;
+}
+
+console.log(nama);
+console.log(usia);
+
+generateBiodata();
